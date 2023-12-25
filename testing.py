af008866b6a9e529b8f09c8ef7046d069a2c1399
Christopher (Ruin21) <118554907+Ruin2121@users.noreply.github.com>
2023-12-25 13:07:45 -0500

    Finished appearance for inventory submenu, might need to be made more robust in the future. Now on to actual functionality and creating per item pages.

---
+++ testing.py
@@ -0,0 +1,21 @@
+from src.items.warframes import WARFRAMES_DICT
+from math import floor
+
+if __name__ == "__main__":
+    # print(len(WARFRAMES_DICT))
+    # print(floor(len(WARFRAMES_DICT) / 4))
+    # print(len(WARFRAMES_DICT) // 4)
+    # print(len(WARFRAMES_DICT) % 4)
+
+    # number_of_items = len(WARFRAMES_DICT)
+    # columns = 4
+    # number_of_rows = (
+    #     (number_of_items // columns) + 1 if (number_of_items % columns) > 0 else 0
+    # )
+
+    # print(number_of_rows)
+
+    print(len("Ash                    "))
+    print(len("# Ash Prime              "))
+    print(len("# Atlas                  "))
+    print(len("# Atlas Prime            "))
